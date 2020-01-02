124067d35c43e0c886160bc1f8ab9b5d3168f139
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-01-02 23:32:19 +0800

    edited serializer

--- users/serializers.py
+++ users/serializers.py
@@ -16,7 +16,7 @@ from .models import (
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = 'id','full_name', 'new_nric', 'old_nric', 'phone', 'email', 'gender', 'occupation', 'profile_picture', 'user_type', 'username', 'is_active'
+        fields = 'id','full_name', 'new_nric', 'old_nric', 'phone', 'tel', 'email', 'gender', 'occupation', 'profile_picture', 'user_type', 'username', 'is_active'
         read_only_fields = ['email', 'id']
 
 class UserOccupationSerializer(serializers.ModelSerializer):
