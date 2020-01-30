47111d35863f20b95b7b23016295946fd2f1fd99
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-01-31 06:36:47 +0800

    cmmt

--- reports/views.py
+++ reports/views.py
@@ -66,7 +66,7 @@ class ReportViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         #fs = FileSystemStorage('/tmp')
         #with fs.open('GHG_Report.pdf') as pdf:
         response = HttpResponse(pdf_file, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment;filename="GHG_Report.pdf"'
+        response['Content-Disposition'] = 'filename="GHG_Report.pdf"'
         #response['Content-Disposition'] = 'attachment;filename="GHG_Report.pdf"'
         #return response
 
